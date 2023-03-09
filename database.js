99f08dab02f808425b05f01e409db992d4e3b1e4
Aravindhabii <abiiaravindhr.it2020@citchennai.net>
2023-03-09 09:04:31 +0530

    changed db

--- database.js
+++ database.js
@@ -1,22 +1,25 @@
-const mysql = require('mysql2')
+const mysql = require("mysql2");
+const dotenv = require("dotenv");
+dotenv.config();
 // import mysql from 'mysql2'
 
 db = mysql.createConnection({
-  host: 'localhost',
-  database: 'mydiary',
-  password: 'root',
-  user: 'root'
-})
+  host: process.env.MYSQLHOST,
+  database: process.env.MYSQLDATABASE,
+  password: process.env.MYSQLPASSWORD,
+  user: process.env.MYSQLUSER,
+  port: process.env.MYSQLPORT,
+});
 
 const connection = () => {
   db.connect((err) => {
     if (err) {
-      console.log(err)
+      console.log(err);
     } else {
-      console.log('Connected to database')
+      console.log("Connected to database");
     }
-  })
-}
+  });
+};
 
-module.exports.db = db
-module.exports.connection = connection
+module.exports.db = db;
+module.exports.connection = connection;
--- index.js
+++ index.js
@@ -1,97 +1,97 @@
 //import statements as common js
-const express = require('express')
-const cors = require('cors')
-const uuid = require('uuid')
+const express = require("express");
+const cors = require("cors");
+const uuid = require("uuid");
 // import userroute from './routes/user.route'
-const db = require('./database').db
-const connection = require('./database').connection
-const schema = require('./schema')
+const db = require("./database").db;
+const connection = require("./database").connection;
+const schema = require("./schema");
 
-const app = express()
-app.use(cors())
-app.use(express.json())
+const app = express();
+app.use(cors());
+app.use(express.json());
 
 function setCookie(name, uuid, expirationDays) {
-  const date = new Date()
-  date.setTime(date.getTime() + expirationDays * 24 * 60 * 60 * 1000)
-  const expires = 'expires=' + date.toUTCString()
-  document.cookie = `${name}=${uuid};${expires};path=/`
+  const date = new Date();
+  date.setTime(date.getTime() + expirationDays * 24 * 60 * 60 * 1000);
+  const expires = "expires=" + date.toUTCString();
+  document.cookie = `${name}=${uuid};${expires};path=/`;
 }
 
-app.get('/', (req, res) => {
-  db.query('SELECT * FROM users', (err, resp) => {
+app.get("/", (req, res) => {
+  db.query("SELECT * FROM users", (err, resp) => {
     if (err) {
-      console.log(err)
+      console.log(err);
     } else {
-      res.send(resp)
+      res.send(resp);
     }
-  })
-})
+  });
+});
 
-app.post('/login', (req, res) => {
-  console.log(req.body)
-  const { email, password } = req.body
-  connection()
+app.post("/login", (req, res) => {
+  console.log(req.body);
+  const { email, password } = req.body;
+  connection();
   db.query(
-    'SELECT * FROM users WHERE email = ? AND password = ?',
+    "SELECT * FROM users WHERE email = ? AND password = ?",
     [email, password],
     (err, resp) => {
       if (err) {
-        res.json(err).status(400)
-        db.end()
+        res.json(err).status(400);
+        db.end();
       } else {
         if (resp.length === 1) {
-          res.json(resp).status(200)
-          db.end()
+          res.json(resp).status(200);
+          db.end();
         } else {
-          res.json(['no user found'])
-          db.end()
+          res.json(["no user found"]);
+          db.end();
         }
       }
     }
-  )
-})
+  );
+});
 
-app.post('/register', (req, res) => {
-  const { email, password, name } = req.body
-  const userid = uuid.v4()
-  console.log(email, password)
-  connection()
+app.post("/register", (req, res) => {
+  const { email, password, name } = req.body;
+  const userid = uuid.v4();
+  console.log(email, password);
+  connection();
   db.query(
-    'INSERT INTO users (userid, email, password, name) VALUES (?,?,?,?)',
+    "INSERT INTO users (userid, email, password, name) VALUES (?,?,?,?)",
     [userid, email, password, name],
     (err, resp) => {
       if (err) {
-        console.log(err)
-        res.json(err).status(400)
-        db.end()
+        console.log(err);
+        res.json(err).status(400);
+        db.end();
       } else {
-        console.log(resp)
-        res.json(resp).status(200)
-        db.end()
+        console.log(resp);
+        res.json(resp).status(200);
+        db.end();
       }
     }
-  )
-})
+  );
+});
 
-app.post('/addentry', (req, res) => {
-  const { userid, title, body } = req.body
-  const entryid = uuid.v4()
-  console.log(userid, title, body)
+app.post("/addentry", (req, res) => {
+  const { userid, title, body } = req.body;
+  const entryid = uuid.v4();
+  console.log(userid, title, body);
   db.query(
-    'INSERT INTO entries (entryid, userid, title, body) VALUES (?,?,?,?)',
+    "INSERT INTO entries (entryid, userid, title, body) VALUES (?,?,?,?)",
     [entryid, userid, title, body],
     (err, resp) => {
       if (err) {
-        console.log(err)
-        res.json(err).status(400)
+        console.log(err);
+        res.json(err).status(400);
       } else {
-        console.log(resp)
-        res.json(resp).status(200)
+        console.log(resp);
+        res.json(resp).status(200);
       }
     }
-  )
-})
+  );
+});
 
 // var ciphertext =
 //     ''
@@ -103,6 +103,6 @@ app.post('/addentry', (req, res) => {
 // console.log(decryptedData)
 
 app.listen(8080, () => {
-  console.log('Server is running on port 8080')
-  schema()
-})
+  console.log("Server is running on port 8080");
+  schema();
+});
